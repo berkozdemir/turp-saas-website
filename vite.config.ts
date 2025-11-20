c44e873ff6317d5581b89c206c1ccba270763065
Berk Özdemir <berkozdemir@gmail.com>
2025-11-20 21:35:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // <--- YENİ EKLENEN KISIM
 
 export default defineConfig({
   plugins: [react()],
+  
+  // Vercel'in doğru dosya yollarını çözmesini sağlayan ayar
+  resolve: {
+    alias: {
+      // @ sembolünü kullanarak src klasörüne hızlı erişim sağlıyoruz.
+      '@': path.resolve(__dirname, './src'), 
+    },
+  },
+  
+  // Çıktı yollarını ve statik varlıkları doğru ayarlıyoruz (genellikle bu zorunlu değildir ama sorunları çözer)
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 });
