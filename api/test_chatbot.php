40a1fc6751e38a27bb339aaafe67c2a85f8597b4
Berk Özdemir <berkozdemir@Berk-MacBook-Pro.local>
2026-01-10 09:25:40 +0300

    fix: Add error reporting to test_chatbot.php

--- api/test_chatbot.php
+++ api/test_chatbot.php
@@ -5,6 +5,11 @@
  * URL: https://iwrs.com.tr/api/test_chatbot.php
  */
 
+// Enable error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json');
 
 // Load env
